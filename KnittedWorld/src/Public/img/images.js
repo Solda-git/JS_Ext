2dbe2a98035b63968798428cb1ad5584892e93a9
Sergey Soldatov <soldatovsl@mail.ru>
2020-08-06 19:41:49 +0300

    footer added

--- KnittedWorld/src/Public/img/images.js
+++ KnittedWorld/src/Public/img/images.js
@@ -9,7 +9,8 @@ import img8 from './IMG_0098_s.jpg';
 import img9 from './background3.jpg';
 import img10 from './background2.jpg';
 import img11 from './background4.png';
+import img12 from './background4.jpg';
 // import img9 from './IMG_0099.jpg';
 // import img10 from './IMG_0099.jpg';
 
-export {img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11}
+export {img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12}

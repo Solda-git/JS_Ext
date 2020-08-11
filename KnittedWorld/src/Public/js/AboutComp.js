36eb4b03959bbe484e87eb2e62dcc9266b6aa630
Sergey Soldatov <soldatovsl@mail.ru>
2020-08-11 17:23:18 +0300

    minor changes, fine tuning of: -AboutComp -index.html -style.css

--- KnittedWorld/src/Public/js/AboutComp.js
+++ KnittedWorld/src/Public/js/AboutComp.js
@@ -11,10 +11,10 @@ const about = {
     template: `
         <div v-if="navAPI.semaphore.about" class="about">
             <div class="about-box">
-                <h2 class="welcome">welcome to</h2>
-                <h1 class="knitted">knitted world</h1>
-                <h3 class="toys-header">авторские вязаные игрушки</h3>
-                <p class="author"><strong>автор и дизайнер - Скороглядова Дарья</strong></p>
+                <h2 class="welcome">WELCOME TO</h2>
+                <h1 class="knitted">"Knitted World"</h1>
+                <h3 class="toys-header">Авторские вязаные игрушки</h3>
+                <p class="author"><strong>Автор и дизайнер - Скороглядова Дарья</strong></p>
                 <p class="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                     Mauris imperdiet ultricies enim vitae mattis. Integer malesuada purus eget eros malesuada fermentum. 
                     Morbi scelerisque metus et quam iaculis condimentum. Quisque at sollicitudin lorem. Vivamus id commodo dui. 
